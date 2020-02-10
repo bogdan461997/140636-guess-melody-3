9090a19f5eb21036781c6f4aa88c3305ed249742
bkostrov <bogdan.kostrov@gmail.com>
2020-02-10 22:39:37 +0300

    Исправил ошибку

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from "./components/app/app.jsx";
 const ERRORS_COUNT = 3;
 
 ReactDOM.render(
-    <App errorsCount={errorsCount} />,
+    <App errorsCount={ERRORS_COUNT} />,
     document.getElementById(`root`)
 );
